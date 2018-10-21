42440d578dd95b27d402560b0b1d5351c8aff6f5
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2018-10-21 20:45:25 +0300

    [ADD] Classes and Instances

---
+++ classes_and_instances.py
@@ -0,0 +1,19 @@
+# Python Object-Oriented Programming
+
+
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+        self.email = first + '.' + last + '@company.com'
+
+    def fullname(self):
+        return '{} {}'.format(self.first, self.last)
+
+
+emp_1 = Employee('Corey', 'Schafer', 50000)
+emp_2 = Employee('Test', 'User', 60000)
+
+print(emp_1.fullname())
+print(Employee.fullname(emp_2))
